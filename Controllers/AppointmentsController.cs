df424e822b1a5c5b89cd5152111720819e9d2bb5
maksymilian <maximustar2006@gmail.com>
2026-04-20 22:18:36 +0200

    Add GET /api/appointments endpoint

---
+++ Controllers/AppointmentsController.cs
@@ -0,0 +1,23 @@
+using Cwiczenia6.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenia6.Controllers;
+
+[ApiController]
+[Route("[api/controller]")]
+public class AppointmentsController : ControllerBase
+{
+    private readonly AppointmentService _appointmentService;
+
+    public AppointmentsController(AppointmentService appointmentService)
+    {
+        _appointmentService = appointmentService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAppointments([FromQuery] string? status, [FromQuery] string? patientLastName)
+    {
+        var appointments = await _appointmentService.GetAppointmentsAsync(status, patientLastName);
+        return Ok(appointments);
+    }
+}
